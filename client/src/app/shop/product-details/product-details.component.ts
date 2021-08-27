23513e3da1ed857f4e127ecf1e1aded0cc5cc2fc
Oleg Donii <odonii@netinfo.md>
2021-08-27 19:10:39 +0300

    - Added routing

---
+++ client/src/app/shop/product-details/product-details.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { IProduct } from 'src/app/shared/models/product';
+import { ShopService } from '../shop.service';
+
+@Component({
+  selector: 'app-product-details',
+  templateUrl: './product-details.component.html',
+  styleUrls: ['./product-details.component.scss']
+})
+export class ProductDetailsComponent implements OnInit {
+  product!: IProduct;
+  
+  constructor(private shopService : ShopService) { }
+
+  ngOnInit(): void {
+    this.loadProduct();
+  }
+
+  loadProduct() {
+    this.shopService.getProduct(2).subscribe(product => {
+      this.product = product;
+    }, error => {
+      console.log(error);
+    })
+  }
+
+}
